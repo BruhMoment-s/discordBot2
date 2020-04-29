09119143baca2e1a304ccf344f61bde9f45db0b6
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-04-29 09:30:21 +0300

    Create update.js

---
+++ src/commands/mod/update.js
@@ -0,0 +1,62 @@
+
+
+module.exports = {
+    run: async(client, message, args) => {
+        if(!message.member.hasPermission('BAN_MEMBERS')) {
+            message.channel.send("You don't have permission to use that command.");
+        }
+        else {
+            try {
+
+           
+                let result = message.replace(/;/ig, ' ');
+              
+                let result2 = message.replace(/|/ig, '\n');
+              
+                      message.delete(0);
+                const embed = {
+                    "content": "" + message.guild.defaultRole.toString(),
+                    "title": "Xedved update!",
+                    "description": "",
+                  
+                    "color": 9936031,
+                    "timestamp": "2020-04-29T05:00:18.664Z",
+                    "footer": {
+                      "icon_url": "https://media.discordapp.net/attachments/703857786496483359/704681488402219038/xedved_thing33.png",
+                      "text": "Xedved update alert"
+                    },
+                  
+                  
+                 
+                    "fields": [
+                 
+                      {
+                        "name": "_ _",
+                        "value": "Version " + Jsoup.connect("https://pastebin.com/raw/RvdaJKih").get().toString(),
+                        "inline": true
+                      },
+                      {
+                        "name": " _ _",
+                        "value": "Update logs:",
+                        "inline": true
+                      },
+                      {
+                        "name": " _ _",
+                        "value": "```" + result2 + "```",
+                        "inline": true
+                      }
+                    ]
+                  };
+                  channel.send({ embed });
+
+
+
+            }
+            catch(err) {
+                console.log(err);
+            }
+        }
+    },
+    aliases: ["Update"],
+    description: 'Bans a guild member by their ID'
+}

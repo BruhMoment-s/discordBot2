dd90939d053540bc863699bb8b44e520f24df267
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-04-29 18:13:37 +0300

    Update update.js

--- src/commands/mod/update.js
+++ src/commands/mod/update.js
@@ -10,50 +10,9 @@ module.exports = {
 
               
                 let justmsg = message.substring(0,11).toString();
-
-
-                let result = justmsg.replace('|', ' ');
-              
-                let result2 = justmsg.replace('|', '\n');
-              
-                    
-                message.channel.send(result2.toString())
-
-
-                const embed = {
-                    "content": "" + message.guild.defaultRole.toString(),
-                    "title": "Xedved update!",
-                    "description": "",
                   
-                    "color": 9936031,
-                    "timestamp": "2020-04-29T05:00:18.664Z",
-                    "footer": {
-                      "icon_url": "https://media.discordapp.net/attachments/703857786496483359/704681488402219038/xedved_thing33.png",
-                      "text": "Xedved update alert"
-                    },
-                  
-                  
-                 
-                    "fields": [
-                 
-                      {
-                        "name": "_ _",
-                        "value": "Version " + Jsoup.connect("https://pastebin.com/raw/RvdaJKih").get().toString(),
-                        "inline": true
-                      },
-                      {
-                        "name": " _ _",
-                        "value": "Update logs:",
-                        "inline": true
-                      },
-                      {
-                        "name": " _ _",
-                        "value": "```" + args[0].toString() + "```",
-                        "inline": true
-                      }
-                    ]
-                  };
-           
+                          message.channel.send(justmsg);
+                
                
                  // message.delete(0);
 

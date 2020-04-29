634fe15c3d71a59722f55f55c90addebc9b486bc
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-04-29 18:12:49 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,23 +1,12 @@
 require('dotenv').config();
-const { Client } = require('discord.js');
-const client = new Client();
-
-client.login(process.env.BOT_TOKEN);
-
-client.on('ready', () => console.log(`${client.user.tag} has logged in.`));
-
-const usersMap = new Map();
-const LIMIT = 5;
-const TIME = 7000;
-const DIFF = 3000;
-let restMEssage;
-client.on('message', message => {
-if (message.substring(0,12) == "_updateText")
-{
- restMEssage = message.substring(0,12).ToString();
-
- message.channel.send(restMEssage);
-}
-
-
-});
+const discord = require('discord.js');
+const client = new discord.Client({ partials: ['MESSAGE', 'REACTION']});
+const { registerCommands, registerEvents } = require('./utils/registry');
+(async () => {
+    client.login(process.env.BOT_TOKEN);
+    client.commands = new Map();
+    client.cachedMessageReactions = new Map();
+    await registerEvents(client, '../events');
+    await registerCommands(client, '../commands');
+    
+})();

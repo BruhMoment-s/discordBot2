661c80373e2a12f5fe74aa732641a773aad9854a
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-04-30 07:42:59 +0300

    Update mute.js

--- src/commands/mod/mute.js
+++ src/commands/mod/mute.js
@@ -1,56 +1,14 @@
 module.exports = {
     run: async(client, message, args) => {
         if(!message.member.hasPermission('BAN_MEMBERS')) {
-            message.channel.send("You don't have permission to use that command.");
+            message.channel.send(args[0].toString());
+           
         }
         else {
             try {
 
               
-                let justmsg = message.substring(0,11).toString();
-
-
-                let result = justmsg.replace('|', ' ');
-              
-                let result2 = justmsg.replace('|', '\n');
-              
-                    
-                message.channel.send(result2.toString())
-
-
-                const embed = {
-                    "content": "" + message.guild.defaultRole.toString(),
-                    "title": "Xedved update!",
-                    "description": "",
-                  
-                    "color": 9936031,
-                    "timestamp": "2020-04-29T05:00:18.664Z",
-                    "footer": {
-                      "icon_url": "https://media.discordapp.net/attachments/703857786496483359/704681488402219038/xedved_thing33.png",
-                      "text": "Xedved update alert"
-                    },
-                  
-                  
-                 
-                    "fields": [
-                 
-                      {
-                        "name": "_ _",
-                        "value": "Version " + Jsoup.connect("https://pastebin.com/raw/RvdaJKih").get().toString(),
-                        "inline": true
-                      },
-                      {
-                        "name": " _ _",
-                        "value": "Update logs:",
-                        "inline": true
-                      },
-                      {
-                        "name": " _ _",
-                        "value": "```" + args[0].toString() + "```",
-                        "inline": true
-                      }
-                    ]
-                  };
+         
            
                
                  // message.delete(0);

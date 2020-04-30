3ed58e14267baf5c9060860c365692097b089d3b
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-04-30 07:58:26 +0300

    Update mute.js

--- src/commands/mod/mute.js
+++ src/commands/mod/mute.js
@@ -4,7 +4,8 @@ module.exports = {
             try {
 
               
-                let justmsg = message.content.substring(0,11).toString();
+                let justmsg = message.content.substring(0,7).toString();
+  message.channel.send(justmsg.toString())
 
 
                 let result = justmsg.replace('|', ' ');
@@ -15,39 +16,6 @@ module.exports = {
                 message.channel.send(result2.toString())
 
 
-                const embed = {
-                    "content": "" + message.guild.defaultRole.toString(),
-                    "title": "Xedved update!",
-                    "description": "",
-                  
-                    "color": 9936031,
-                    "timestamp": "2020-04-29T05:00:18.664Z",
-                    "footer": {
-                      "icon_url": "https://media.discordapp.net/attachments/703857786496483359/704681488402219038/xedved_thing33.png",
-                      "text": "Xedved update alert"
-                    },
-                  
-                  
-                 
-                    "fields": [
-                 
-                      {
-                        "name": "_ _",
-                        "value": "Version " + Jsoup.connect("https://pastebin.com/raw/RvdaJKih").get().toString(),
-                        "inline": true
-                      },
-                      {
-                        "name": " _ _",
-                        "value": "Update logs:",
-                        "inline": true
-                      },
-                      {
-                        "name": " _ _",
-                        "value": "```" + args[0].toString() + "```",
-                        "inline": true
-                      }
-                    ]
-                  };
            
                
                  // message.delete(0);
@@ -58,6 +26,6 @@ module.exports = {
             }
         
     },
-    aliases: ["announce"],
+    aliases: ["update"],
     description: 'Bans a guild member by their ID'
 }
